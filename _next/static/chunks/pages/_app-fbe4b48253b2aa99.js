76511986646e936e4635b4e8af782449351e999b
simülakr <147257658+ivmelenen@users.noreply.github.com>
2025-07-05 16:15:39 +0300

    hello world

---
+++ _next/static/chunks/pages/_app-fbe4b48253b2aa99.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{86614:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(26494)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[774,179],function(){return _(86614),_(19535)}),_N_E=n.O()}]);
