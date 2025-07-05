8b895380f776876aa0d813e5734f716b2dbae3fb
simülakr <ivmelenen@gmail.com>
2025-07-05 22:11:44 +0300

    some changes idk

---
+++ _next/static/chunks/app/blog/[slug]/not-found-1d64a598bd88ce39.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[64,404],{15698:function(n,e,u){Promise.resolve().then(u.t.bind(u,66443,23))}},function(n){n.O(0,[443,24,240,744],function(){return n(n.s=15698)}),_N_E=n.O()}]);
--- _next/static/chunks/app/blog/[slug]/not-found-a76787ff7c88dc02.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[64,404],{4681:function(n,e,u){Promise.resolve().then(u.t.bind(u,66443,23))}},function(n){n.O(0,[443,24,240,744],function(){return n(n.s=4681)}),_N_E=n.O()}]);
